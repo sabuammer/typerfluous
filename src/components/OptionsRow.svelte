b1c301b146a3157044ca95508ba9bc31d2cd0cb9
Salem Abuammer <sabuammer@Salems-MacBook-Pro.local>
2020-06-17 22:47:23 -0500

    Added timed test functionality

---
+++ src/components/OptionsRow.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let options; // List of options
+  export let selectedIdx = 0; // The index of the selected option. Defaults to 0
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick(selected) {
+    selectedIdx = selected;
+    dispatch("select", {
+      selectedIdx
+    });
+  }
+</script>
+
+<style>
+  div {
+    display: flex;
+  }
+
+  button {
+    font-family: "Quicksand", sans-serif;
+    font-size: 1.5em;
+    outline: none;
+    background: transparent;
+    border: none;
+    color: #333;
+  }
+
+  button:hover {
+    background-color: #333;
+    color: white;
+  }
+
+  .selected {
+    font-weight: bold;
+  }
+</style>
+
+<div>
+  {#each options as option, i}
+    <button class:selected={i === selectedIdx} on:click={() => handleClick(i)}>
+      {option}
+    </button>
+  {/each}
+</div>
--- src/components/TypingTest.svelte
+++ src/components/TypingTest.svelte
@@ -2,8 +2,12 @@
   import { fade } from "svelte/transition";
   import { onMount } from "svelte";
   import randomWords from "random-words";
+  import OptionsRow from "./OptionsRow.svelte";
 
   const wordCounts = [10, 50, 100, 150, 200];
+  const testCategories = ["words", "timed"];
+  const testingTimes = [0.5, 1, 2, 3, 5];
+  const timedTestWordCount = 1000;
 
   // DOM bindings
   let typingInput; // Reference to input that user types in
@@ -17,12 +21,16 @@
   let classes = []; // The class for each individual word in the paragraph (selected or not, correct or not)
   let charsTyped = ""; // All correct characters typed by the user so far
   let inputIndex = 0; // Index into the words array. Starts at zero before user begins typing
-  let selectedIdx = 1; // default index into wordCounts array
+  let selectedCountsIdx = 1; // default index into wordCounts array
+  let selectedTimesIdx = 1;
+  let numErrors = 0;
 
   let timerInterval;
   let secondsElapsed = 0;
 
-  let notTyping = true; // Whether or not user has begun typing
+  let typing = false; // Whether or not user has begun typing
+  let disableTyping = false; // Whether or not we should prevent any more calls to handleInput
+  let wordsTest = true; // Type of typing test, default to words test, false means timed test
 
   // Lifecycle methods
   onMount(() => {
@@ -30,33 +38,68 @@
   });
 
   // Reactive variables
+  $: numWords = wordCounts[selectedCountsIdx]; // Number of words user has selected to type
+  $: timeLimit = testingTimes[selectedTimesIdx] * 60; // Number of seconds alotted for timed typing test
+  $: timeRemaining = new Date(1000 * timeLimit).toISOString().slice(15, -5); // String detailing the time left in timed test in human readable format
   $: words = randomWords(numWords); // The words generated for later typing
-  $: avgWordLen =
-    words
-      .map(word => word.length)
-      .reduce((accum, currLength) => accum + currLength) / words.length; // For use in calculating average word length for wpm calc
   $: totalChars = words.join("");
-  $: numWords = wordCounts[selectedIdx]; // Number of words user has selected to type
+
+  function handleWordsTest(e) {
+    if (!typing) {
+      timerInterval = setInterval(() => secondsElapsed++, 1000);
+      typing = true;
+    }
+
+    handleInput(e);
+
+    if (inputIndex === numWords) {
+      clearInterval(timerInterval);
+      secondsElapsed = 0;
+      typing = false;
+      disableTyping = true;
+    }
+  }
+
+  function handleTimedTest(e) {
+    if (!typing) {
+      timerInterval = setInterval(() => {
+        timeLimit--;
+        secondsElapsed++; // Increment here so that wpm calculation works for timed test as well
+      }, 1000);
+      typing = true;
+    }
+
+    handleInput(e);
+
+    if (timeLimit === 0) {
+      clearInterval(timerInterval);
+      timeLimit = testingTimes[selectedTimesIdx] * 60;
+      typing = false;
+      disableTyping = true;
+    }
+  }
 
   function handleInput(e) {
-    if (notTyping) {
-      timerInterval = setInterval(() => secondsElapsed++, 1000);
-      notTyping = false;
-    }
-    if (e.data === " ") {
-      if (currTyped.trim() === words[inputIndex]) {
-        charsTyped += currTyped.trim();
+    if (e.key === " ") {
+      let currInput = currTyped ? currTyped.trim() : ""; // This makes sure that something is actually typed into the input
+      if (currInput === words[inputIndex]) {
         classes.push("correct");
       } else {
+        numErrors += words[inputIndex].length;
         classes.push("incorrect");
       }
-      if (secondsElapsed > 0) {
-        accuracy = (charsTyped.length / totalChars.length) * 100;
-        netWPM = charsTyped.length / avgWordLen / (secondsElapsed / 60);
+
+      charsTyped += words[inputIndex];
+
+      if (secondsElapsed > 0 && charsTyped.length > 0) {
+        // This check is to make sure accuracy doesn't perform a division by 0
+        accuracy = ((charsTyped.length - numErrors) / charsTyped.length) * 100;
+        netWPM = (charsTyped.length - numErrors) / 5 / (secondsElapsed / 60);
       }
+
       currTyped = "";
-			inputIndex++;
-			
+      inputIndex++;
+
       if (inputIndex < numWords) {
         let nextWord = paragraphText.childNodes[inputIndex];
         let scrollingOffset = nextWord.offsetTop - paragraphText.offsetTop;
@@ -74,36 +117,40 @@
           numScroll++;
         }
       }
-		}
-		
-    if (inputIndex === numWords) {
-      clearInterval(timerInterval);
-      secondsElapsed = 0;
-      notTyping = true;
-      typingInput.disabled = true;
     }
   }
 
-  function handleWordCountChange(newNumWords) {
-    numWords = newNumWords;
-    selectedIdx = wordCounts.indexOf(newNumWords);
-		handleRestart();
+  function handleTimeLimitChange(e) {
+    selectedTimesIdx = e.detail.selectedIdx;
+    numWords = timedTestWordCount;
+    handleRestart();
+  }
+
+  function handleWordCountChange(e) {
+    selectedCountsIdx = e.detail.selectedIdx;
+    handleRestart();
+  }
+
+  function handleTestCategoryChange(e) {
+    wordsTest = e.detail.selectedIdx === 0;
+    numWords = wordsTest ? wordCounts[selectedCountsIdx] : timedTestWordCount;
+    handleRestart();
   }
 
   function handleRestart() {
     inputIndex = 0;
+    numErrors = 0;
     currTyped = "";
     charsTyped = "";
     classes = [];
     words = randomWords(numWords);
+    timeLimit = testingTimes[selectedTimesIdx] * 60;
     accuracy = 0;
     netWPM = 0;
     secondsElapsed = 0;
-    notTyping = true;
-    if (timerInterval) {
-      clearInterval(timerInterval);
-    }
-    typingInput.disabled = false;
+    typing = false;
+    disableTyping = false;
+    clearInterval(timerInterval);
     paragraphText.scrollTo(0, 0);
     numScroll = 0;
     typingInput.focus();
@@ -111,8 +158,19 @@
 
   function handleKeydown(e) {
     // Add special key shortcuts here
-    if (e.key === "Escape" || e.key === "Esc") {
-      handleRestart();
+    switch (e.key) {
+      case "Escape":
+      case "Esc":
+        handleRestart();
+        break;
+      default:
+        if (!disableTyping) {
+          if (wordsTest) {
+            handleWordsTest(e);
+          } else {
+            handleTimedTest(e);
+          }
+        }
     }
   }
 </script>
@@ -135,6 +193,15 @@
     scrollbar-color: #333 transparent;
   }
 
+  p::-webkit-scrollbar {
+    background-color: transparent;
+  }
+
+  p::-webkit-scrollbar-thumb {
+    background-color: #333;
+    border-radius: 10px;
+  }
+
   span {
     margin-right: 0.3em;
   }
@@ -152,7 +219,7 @@
     color: white;
   }
 
-  .selected-counts {
+  .selected-option {
     font-weight: bold;
   }
 
@@ -205,8 +272,9 @@
     margin-top: 20px;
   }
 
-  .options-row {
+  .options-and-info-row {
     display: flex;
+    justify-content: space-between;
     margin-top: 15px;
   }
 
@@ -218,21 +286,30 @@
   .result-labels {
     margin-left: auto;
   }
+
+  .type-labels {
+    margin: 0 auto;
+  }
 </style>
 
 <div transition:fade={{ delay: 250 }}>
-  <div class="options-row">
-    {#each wordCounts as wordCount, i}
-      <span
-        class={i === selectedIdx ? ['selected-counts', 'word-counts-span'].join(' ') : 'word-counts-span'}
-        on:click={() => handleWordCountChange(wordCount)}>
-        {wordCount}
-      </span>
-    {/each}
-    <div class="result-labels">
+  <div class="options-and-info-row">
+    {#if wordsTest}
+      <OptionsRow
+        selectedIdx={selectedCountsIdx}
+        options={wordCounts}
+        on:select={handleWordCountChange} />
+    {:else}
+      <OptionsRow
+        selectedIdx={selectedTimesIdx}
+        options={testingTimes}
+        on:select={handleTimeLimitChange} />
+    {/if}
+    <OptionsRow options={testCategories} on:select={handleTestCategoryChange} />
+    <div>
       <span class="options-label">
         <strong>net wpm:</strong>
-        {Math.round(netWPM)} |
+        {Math.round(netWPM)}
       </span>
       <span class="options-label">
         <strong>acc:</strong>
@@ -254,10 +331,9 @@
         type="text"
         bind:this={typingInput}
         bind:value={currTyped}
-        on:input={handleInput}
         on:keydown={handleKeydown} />
-      <button class="restart-btn" on:click={() => handleRestart()}>
-        restart
+      <button class="restart-btn" on:click={handleRestart}>
+        {#if wordsTest}restart{:else}{timeRemaining}{/if}
       </button>
     </div>
   </div>
